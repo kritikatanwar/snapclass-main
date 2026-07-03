679c1bf3c275d7eb167d66141b88552acf428b28
Kritika <tanwar.kritika3010@gmail.com>
2026-07-03 23:33:00 +0530

    voice attendance error fix

--- src/components/dialog_attendance_results.py
+++ src/components/dialog_attendance_results.py
@@ -3,27 +3,29 @@ from src.database.config import supabase
 from src.database.db import create_attendance
 
 
-
-def show_attendance_result(df,logs):
+def show_attendance_result(df, logs):
     st.write("Please review attendance before confirming")
-    st.dataframe(df,hide_index=True, width='stretch')
+    st.dataframe(df, hide_index=True, width='stretch')
 
-    col1,col2= st.columns(2)
+    col1, col2 = st.columns(2)
     with col1:
         if st.button('Discard', width='stretch'):
-            st.session_state.attendance_resuls= None
+            st.session_state.voice_attendance_results = None
             st.rerun()
+
     with col2:
-        if st.button('Confirm & Save', width='stretch',type= 'primary'):
+        if st.button('Confirm & Save', width='stretch', type='primary'):
             try:
                 create_attendance(logs)
                 st.toast("Attendance Taken!")
-                st.session_state.attendance_images=[]
-                st.session_state.attendance_resuls= None
+                st.session_state.attendance_images = []
+                st.session_state.voice_attendance_results = None
                 st.rerun()
             except Exception as e:
-               st.error('Sync Failed')
-               st.rerun()
+                st.error('Sync Failed')
+                st.rerun()
+
+
 @st.dialog("Attendance Reports")
-def attendance_results_dialog(df,logs):
-  show_attendance_result(df,logs)
+def attendance_results_dialog(df, logs):
+    show_attendance_result(df, logs)
